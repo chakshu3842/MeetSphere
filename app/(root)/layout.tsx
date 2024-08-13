f8982aad1e34b931b1d1a3bc4d736d9acd53f731
Chakshu <chakshushandilya@gmail.com>
2024-08-14 00:22:12 +0530

    added instant meeting room and created differant ids for meeting room and added token from shadcn in meeting room for instant meeting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,12 @@
-import React, { ReactNode } from 'react'
+import StreamVideoProvider from "@/providers/streamClientProvider";
+import React, { ReactNode } from "react";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      {children}
+      <StreamVideoProvider>{children}</StreamVideoProvider>
     </main>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
